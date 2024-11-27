f329bf3fcd2eee118d310f109345604b44ce321e
Facundo Trompo <facundo@trompoagencia.com>
2024-11-27 15:48:25 -0300

    Estructura básica y rutas básicas de páginas

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+// import About from '../pages/About';
+// import Contact from '../pages/Contact';
+// import NotFound from '../pages/NotFound';
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Páginas principales */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Ruta para páginas secundarias */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
